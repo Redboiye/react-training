fea4cf552caaf17a12361fd3c10a34d538ea0f82
Kaspars <smarnss@gmail.com>
2024-11-28 20:53:43 +0200

    Add simple features

--- cute-mama/src/HelloWorld.jsx
+++ cute-mama/src/HelloWorld.jsx
@@ -1,19 +1,47 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 export const HelloWorld = (props) => {
     const [count, setCount] = useState(0)
+    let emoji = null
+    useEffect(() => {
+        if (count === 10) {
+            alert("Congrats you reached 10 likes")
+        }
+    }, [count]);
+    useEffect(() => {
+        if (count === 0) {
+            document.title = "Negative is for pussies"
+        }
+    }, [count]);
+    const incrementCount = () => {
+        setCount(count + 1)
+    }
+    const decrementCount = () => {
+        setCount(count - 1)
+    }
+    const refreshCount = () => {
+        setCount(0)
+    }
+    if (count >= 10) {
+        emoji = "🤩"
+    } else if (count < 10 && count >= 1) {
+        emoji = "🤮"
+    } else {
+        emoji = "😭"
+    }
+
     return (
         <div>
             <h1>Hello {props.name}</h1>
             <p>Like Count {count}</p>
-            <button onClick={
-                () => {
-                    setCount(count+1)
-                }
-            }>
-                Give Like
+            <button onClick={incrementCount}>Give Like</button>
+            <button onClick={refreshCount}>Refresh</button>
 
-            </button>
+            <button onClick={decrementCount}>Take Like Back</button>
+
+            <p style={{fontSize: "2em"}}>{emoji}</p>
+
+
         </div>
     )
 
